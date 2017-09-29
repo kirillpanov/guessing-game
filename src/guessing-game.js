ee69ad3f4117ea34880c73d7ad54ffb75fed7099
kirillpanov <kirill.panou@gmail.com>
2017-09-29 06:32:37 +0300

    done and tested

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,45 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number) {
+        this.range = [];
+        this.number = number;
+    }
 
     setRange(min, max) {
-
-    }
+        let numberOfElementsToDelete;
+        for (let i = min; i <= max; i++) {
+            this.range.push(i);
+        };
+    };
 
     guess() {
-
+        let sumOfArr = this.range.reduce(function(a, b) {
+            return a + b;
+        });
+        return Math.round (sumOfArr / this.range.length);
     }
-
+    
     lower() {
-
+        let breakPoint,
+            numberOfElementsToDelete;
+        breakPoint = Math.round ( (this.range.length + 1 ) / 2 );
+        if (this.range.length % 2) {
+            numberOfElementsToDelete = (this.range.length - 1) / 2;
+        } else {
+            numberOfElementsToDelete = this.range.length / 2;
+        };
+        this.range.splice( breakPoint , numberOfElementsToDelete  );
     }
 
     greater() {
-
+        let numberOfElementsToDelete;
+        if (this.range.length % 2) {
+            numberOfElementsToDelete = (this.range.length - 1) / 2;
+        } else {
+            numberOfElementsToDelete = this.range.length / 2;
+        };
+        this.range.splice( 0 , numberOfElementsToDelete  );
     }
+
 }
 
 module.exports = GuessingGame;
